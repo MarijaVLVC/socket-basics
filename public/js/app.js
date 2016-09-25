0909bc3b029e90ff82e6332de39577b52b268661
Marija Vulovic <marija@vulovic.rs>
2016-09-25 22:46:15 +0200

    Added Custum message

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to Socket fe');
 });
+
+socket.on('message', function (message) {
+	console.log('New message');
+	console.log(message.text);
+});
